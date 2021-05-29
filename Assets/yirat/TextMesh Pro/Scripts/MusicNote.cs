7ff98fcecd888321e8e430ee4fe592cdc705a399
yiratpeleg <yiratpeleg8@gmail.com>
2021-05-30 00:36:31 +0300

    change the musicNote Script

---
+++ Assets/yirat/TextMesh Pro/Scripts/MusicNote.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicNote : MonoBehaviour
+{
+    [SerializeField] AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(notePlay);
+    }
+    public void notePlay()
+    {
+        audioSource.Play();
+    }
+}
