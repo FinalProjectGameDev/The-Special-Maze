0f2108e63294cba3a5f69a6c1f439a9baffabcd5
yiratpeleg <yiratpeleg8@gmail.com>
2021-08-08 22:47:58 +0300

    adding tutorial

---
+++ Assets/yirat/TextMesh Pro/Scripts/Tutorial.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Tutorial
+{
+    public string description;
+    public Image keys;
+    public KeyCode[] keysToPlay;
+}
---
+++ Assets/yirat/TextMesh Pro/Scripts/TutorialManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class TutorialManager : MonoBehaviour
+{
+    private int tutorialIndex;
+    private int keysToPlayIndedx;
+
+    public Canvas tutorialCanvas;
+    public Tutorial[] regularTutorials;
+    public Tutorial[] wheelchairTutorials;
+    public TMP_Text currentDescription;
+    public Image currentKeys;
+
+    private void Start()
+    {
+        tutorialIndex = 0;
+        keysToPlayIndedx = 0;
+    }
+
+    void Update()
+    {
+        if (tutorialIndex < regularTutorials.Length)
+        {
+            if (Input.GetKey(regularTutorials[tutorialIndex].keysToPlay[keysToPlayIndedx]))
+            {
+
+                if (keysToPlayIndedx == regularTutorials[tutorialIndex].keysToPlay.Length - 1)
+                {
+                    UpdateCanvas();
+                }
+
+                else keysToPlayIndedx++;
+            }
+        }
+
+    }
+
+    public void UpdateCanvas()
+    {
+        if (tutorialIndex < regularTutorials.Length - 1)
+        {
+            tutorialIndex++;
+            keysToPlayIndedx = 0;
+            currentDescription.text = regularTutorials[tutorialIndex].description;
+            currentKeys.gameObject.SetActive(false);
+            currentKeys = regularTutorials[tutorialIndex].keys;
+            currentKeys.gameObject.SetActive(true);
+        }
+        else tutorialCanvas.gameObject.SetActive(false);    
+    }
+}
